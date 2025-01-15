33f11bf1114dbf29de95f9ac192c7dc35d2c3db9
Jemin <jemsworld@gmail.com>
2025-01-15 17:34:42 +0000

    Updates

--- src/src/content/config.ts
+++ src/src/content/config.ts
@@ -1,28 +1,32 @@
 import { defineCollection, z } from 'astro:content';
 
+const articleSchema = z.object({
+	title: z.string(),
+	description: z.string(),
+	category: z.enum(['Architect', 'Back End', 'Dev Ops', 'DIY', 'Front End', 'General', 'Temp']),		
+	publishedDate: z.coerce.date(),
+	updatedDate: z.coerce.date().optional(),
+	draft: z.boolean().optional()
+
+	// tags: z.array(z.enum(['Front End','Back End','DIY'])),
+	// tags: z.array(z.string()),
+});
+
 const articleColleection = defineCollection({
 	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		category: z.enum(['Architect', 'Back End', 'Dev Ops', 'DIY', 'Front End', 'General', 'Temp']),
-		// tags: z.array(z.enum(['Front End','Back End','DIY'])),
-		// tags: z.array(z.string()),
-		publishedDate: z.coerce.date(),	// Transform string to Date object
-		updatedDate: z.coerce.date().optional()
-	}),
+	schema: articleSchema,
 });
 
 const snippetCollection = defineCollection({
 	type: 'content',
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		lang: z.enum(['CSS', 'JavaScript', 'C Sharp', 'SQL']),
 		description: z.string().optional(),
 		publishedDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional()
+		updatedDate: z.coerce.date().optional(),
+		draft: z.boolean().optional()
+
 	}),
 });
 
@@ -30,3 +34,10 @@ export const collections = {
 	'article': articleColleection,
 	'snippet': snippetCollection,
 };
+
+
+// Define the base type inferred from the schema
+type ArticleData = z.infer<typeof articleSchema>;
+
+// Extend the type to include `slug`
+export type Article = ArticleData & { slug: string };
---
+++ src/src/utils/helper.ts
@@ -0,0 +1,9 @@
+import { getCollection } from 'astro:content';
+
+export async function getPublishedArticles() {
+  return await getCollection("article", ({ data }) => !data.draft);
+}
+
+export async function getPublishedSnippets() {
+    return await getCollection("snippet", ({ data }) => !data.draft);
+  }
