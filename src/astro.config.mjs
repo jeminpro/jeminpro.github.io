c6af9d02d8c069542a83b03eae94b92cb5679cbc
Jemin <jemsworld@gmail.com>
2025-01-30 16:28:13 +0000

    Astro icons

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -4,11 +4,13 @@ import sitemap from '@astrojs/sitemap';
 
 import expressiveCode from "astro-expressive-code";
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   // site: 'http://localhost:4321',
   site: 'https://jeminpro.com',
   integrations: [expressiveCode({
     themes: ['dark-plus'],
-  }), mdx(), sitemap()]
+  }), mdx(), sitemap(), icon()]
 });
