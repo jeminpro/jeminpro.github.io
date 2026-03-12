57f0c29faa06010f441e1016911fdefbac57ba4e
Jemin <jemsworld@gmail.com>
2026-03-12 14:44:39 +0000

    Deployment version and force use node 24

--- app/src/pages/rss.xml.js
+++ app/src/pages/rss.xml.js
@@ -3,6 +3,10 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
+	const runNumber = process.env.GITHUB_RUN_NUMBER;
+	const shortSha = process.env.GITHUB_SHA?.slice(0, 7);
+	const deploymentVersion = runNumber ? `gha-${runNumber}${shortSha ? ` (${shortSha})` : ''}` : 'local-build';
+
 	const posts = await getCollection('article');
 	const postItems = posts.map((post) => ({
 		...post.data,
@@ -13,7 +17,7 @@ export async function GET(context) {
 
 	return rss({
 		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
+		description: `${SITE_DESCRIPTION} (Deploy: ${deploymentVersion})`,
 		site: context.site,
 		trailingSlash:false,
 		items: [...postItems],
